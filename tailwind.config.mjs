033ea04ffc33bc43ea1287582a4cfd3d46772247
Eur1p3des <nestanguera@gmail.com>
2024-08-14 16:34:04 +0200

    chore: Update font size for tags in Projects.astro and Hero.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "2xs": ".5rem",
+      },
+    },
   },
   plugins: [],
 };
